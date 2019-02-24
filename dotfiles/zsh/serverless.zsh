9cdd274e86b99e459a1ef68a15b5de91352e05c5
Richard Allen <richard.l.allen@gmail.com>
2019-02-24 10:37:23 -0500

    Serverless tab completion setup

--- dotfiles/zsh/serverless.zsh
+++ dotfiles/zsh/serverless.zsh
@@ -1,6 +1,10 @@
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[[ -f /Users/rich/.nvm/versions/node/v10.8.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh ]] && . /Users/rich/.nvm/versions/node/v10.8.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[[ -f /Users/rich/.nvm/versions/node/v10.8.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh ]] && . /Users/rich/.nvm/versions/node/v10.8.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh
+function slstab {
+  nvm use node
+  NPM_ROOT=$(npm root -g)
+  SVRLS="${NPM_ROOT}/serverless/node_modules/tabtab/.completions/serverless.zsh"
+  SLS="${NPM_ROOT}/serverless/node_modules/tabtab/.completions/sls.zsh"
+
+  [[ -f "${SVRLS}" ]] && . "${SVRLS}"
+  [[ -f "${SLS}" ]] && . "${SLS}"
+}
+
